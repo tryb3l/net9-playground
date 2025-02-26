ca3de5b80b34066b36194cc89336f227c35c9f73
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-02-26 17:46:38 +0100

    migrations

--- WebApplication1/Data/Migrations/20250226161105_AddMoreSeedAdmin.Designer.cs
+++ WebApplication1/Data/Migrations/20250226161105_AddMoreSeedAdmin.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WebApplication1.Data;
@@ -11,9 +12,11 @@ using WebApplication1.Data;
 namespace WebApplication1.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250226161105_AddMoreSeedAdmin")]
+    partial class AddMoreSeedAdmin
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
