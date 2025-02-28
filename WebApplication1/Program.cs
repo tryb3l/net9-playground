8fcffc801775f6d91cc670c868c4f6ba83437583
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-02-28 22:13:16 +0100

    add admin route mapping to Program.cs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -73,6 +73,11 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
+    name: "admin",
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+
+app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
