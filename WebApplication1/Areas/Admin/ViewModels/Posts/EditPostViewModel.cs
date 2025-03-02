bf30fc515b073e603c63106dcf63f0415d73f1c9
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-03-02 23:58:00 +0100

    add EditPostViewModel for editing posts in admin area

---
+++ WebApplication1/Areas/Admin/ViewModels/Posts/EditPostViewModel.cs
@@ -0,0 +1,31 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApplication1.Areas.Admin.ViewModels.Posts;
+
+public class EditPostViewModel
+{
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Title is required")]
+    [StringLength(255, MinimumLength = 2, ErrorMessage = "Title must be between 2 and 255 characters")]
+    public string Title { get; set; } = string.Empty;
+
+    [Display(Name = "Content")]
+    public string? Content { get; set; }
+
+    [Display(Name = "Published")]
+    public bool IsPublished { get; set; }
+
+    [Display(Name = "Publication Date")]
+    [DataType(DataType.DateTime)]
+    public DateTime? PublishedDate { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    [Display(Name = "Tags")]
+    public List<int> SelectedTagIds { get; set; } = new List<int>();
+
+    public List<SelectListItem> AvailableTags { get; set; } = new List<SelectListItem>();
+}
