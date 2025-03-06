3bca72b948322a8c86bd5ab461d3a7698c983005
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-03-06 22:58:04 +0100

    update views

---
+++ WebApplication1/Views/Blog/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Views.Blog
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
