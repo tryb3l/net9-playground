87c0567cdf8a78e264cdfe8cfd003ebdc96d7116
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-03-06 22:59:41 +0100

    update post repository

---
+++ WebApplication1/Repositories/PostRepository.cs
@@ -0,0 +1,81 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Interfaces;
+using WebApplication1.Models;
+
+namespace WebApplication1.Repositories;
+
+public class PostRepository : IPostRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public PostRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Post?> GetByIdAsync(int id)
+    {
+        return await _context.Posts.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Post>> GetAllAsync()
+    {
+        return await _context.Posts.ToListAsync();
+    }
+
+    public async Task AddAsync(Post entity)
+    {
+        await _context.Posts.AddAsync(entity);
+    }
+
+    public Task UpdateAsync(Post entity)
+    {
+        _context.Posts.Update(entity);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(Post entity)
+    {
+        _context.Posts.Remove(entity);
+        return Task.CompletedTask;
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Post>> GetPublishedPostsAsync(int skip, int take)
+    {
+        return await _context.Posts
+            .Include(p => p.Author)
+            .Include(p => p.Category)
+            .Include(p => p.PostTags)
+            .ThenInclude(pt => pt.Tag)
+            .Where(p => p.IsPublished)
+            .OrderByDescending(p => p.PublishedDate)
+            .Skip(skip)
+            .Take(take)
+            .ToListAsync();
+    }
+
+    public async Task<int> CountPublishedPostsAsync()
+    {
+        return await _context.Posts
+            .Where(p => p.IsPublished)
+            .CountAsync();
+    }
+
+    public async Task<Post> GetPostWithDetailsAsync(int id)
+    {
+        var post = await _context.Posts
+        .Include(p => p.Author)
+        .Include(p => p.Category)
+        .Include(p => p.PostTags)
+        .ThenInclude(pt => pt.Tag)
+        .FirstOrDefaultAsync(p => p.Id == id);
+
+        return post ?? throw new KeyNotFoundException($"Post with id {id} not found");
+    }
+}
