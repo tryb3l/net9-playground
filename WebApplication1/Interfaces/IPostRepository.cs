237a6b7ccd0754f947a6ed3bc7d45f16295fcc76
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-03-07 22:40:46 +0100

    extend post repository interface

--- WebApplication1/Interfaces/IPostRepository.cs
+++ WebApplication1/Interfaces/IPostRepository.cs
@@ -8,4 +8,7 @@ public interface IPostRepository : IRepository<Post>
     Task<IEnumerable<Post>> GetPublishedPostsAsync(int skip, int take);
     Task<Post> GetPostWithDetailsAsync(int id);
     Task<int> CountPublishedPostsAsync();
+    Task<IEnumerable<Post>> GetPostsWithFiltersAsync(string? searchTerm, string? tagFilter, bool? publishedOnly, int skip, int take);
+    Task<int> CountPostsWithFiltersAsync(string? searchTerm, string? tagFilter, bool? publishedOnly);
+    Task DeletePostTagsAsync(IEnumerable<PostTag> postTags);
 }
