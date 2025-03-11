7faaa6a0a2f64b3997f8235acfee2a41b3ed49ab
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-03-11 23:57:53 +0100

    add IAccountService interface for user authentication operations

---
+++ WebApplication1/Interfaces/IAccountService.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Identity;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Interfaces;
+
+public interface IAccountService
+{
+    Task<SignInResult> SignInUserAsync(string email, string password, bool rememberMe);
+    Task<IdentityResult> RegisterUserAsync(RegisterViewModel model);
+    Task SignOutAsync();
+    Task<ExternalLoginInfo> GetExternalLoginInfoAsync();
+    Task<SignInResult> ExternalLoginSignInAsync(string loginProvider, string providerKey, bool isPersistent);
+    Task<IdentityResult> CreateUserWithExternalLoginAsync(ExternalLoginInfo info);
+}
