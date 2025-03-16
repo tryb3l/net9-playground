bc57e907655abf747f3b3c0715eab651644af2c6
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-03-16 23:53:12 +0100

    imports optimizations

--- WebApplication1/Controllers/BlogController.cs
+++ WebApplication1/Controllers/BlogController.cs
@@ -1,40 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using WebApplication1.Interfaces;
-using WebApplication1.Services;
-using WebApplication1.ViewModels;
 
 namespace WebApplication1.Controllers;
 
-    public class BlogController : Controller
+public class BlogController : Controller
+{
+    private readonly IPostService _postService;
+
+    public BlogController(IPostService postService)
     {
-        private readonly IPostService _postService;
-
-        public BlogController(IPostService postService)
-        {
-            _postService = postService;
-        }
-
-        public async Task<ActionResult> Index(int page = 1, string? category = null, string? tag = null)
-        {
-            var viewModel = await _postService.GetBlogIndexViewModelAsync(page, category, tag);
-            return View(viewModel);
-        }
-
-        public async Task<IActionResult> Post(int id, string? slug)
-        {
-            var post = await _postService.GetPostByIdAsync(id);
-
-            if(post == null)
-            {
-                return NotFound();
-            }
-
-            if(!string.IsNullOrEmpty(post.Slug) && slug != post.Slug)
-            {
-                return RedirectToAction(nameof(Post), new { id = post.Id, slug = post.Slug});
-            }
-
-            return View(post);
-        }
+        _postService = postService;
     }
+
+    public async Task<ActionResult> Index(int page = 1, string? category = null, string? tag = null)
+    {
+        var viewModel = await _postService.GetBlogIndexViewModelAsync(page, category, tag);
+        return View(viewModel);
+    }
+
+    public async Task<IActionResult> Post(int id, string? slug)
+    {
+        var post = await _postService.GetPostByIdAsync(id);
+
+        if (post == null)
+        {
+            return NotFound();
+        }
+
+        if (!string.IsNullOrEmpty(post.Slug) && slug != post.Slug)
+        {
+            return RedirectToAction(nameof(Post), new { id = post.Id, slug = post.Slug });
+        }
+
+        return View(post);
+    }
+}
