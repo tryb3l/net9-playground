05649c68065e21d8e4175afdb3311a52d9bc465a
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-03-20 01:01:28 +0100

    small tweaks

--- WebApplication1/entrypoint.sh
+++ WebApplication1/entrypoint.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 set -e
 
-# Wait for the database
 echo "Waiting for PostgreSQL to start..."
+
+if ! command -v pg_isready > /dev/null; then
+  echo "Installing PostgreSQL client..."
+  apk add --no-cache postgresql-client
+fi
+
 postgres_ready() {
   pg_isready -h postgres -U postgres
 }
 
 until postgres_ready; do
+  echo "PostgreSQL is unavailable - sleeping"
   sleep 1
 done
 echo "PostgreSQL is up - continuing"
 
-echo "Running database migrations..."
-cd /app
-dotnet ef database update
-
-# Start the application
 exec dotnet WebApplication1.dll
