0ab99d13c0aaec97f59d45cd27f27ac133d96008
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-03-26 17:09:01 +0100

    implement soft delete properties in Post model

--- WebApplication1/Models/Post.cs
+++ WebApplication1/Models/Post.cs
@@ -1,8 +1,9 @@
 using System;
+using WebApplication1.Interfaces;
 
 namespace WebApplication1.Models;
 
-public class Post
+public class Post : ISoftDelete
 {
     public int Id { get; set; }
     public required string Title { get; set; }
@@ -11,6 +12,8 @@ public class Post
     public DateTime CreatedAt { get; set; }
     public DateTime? PublishedDate { get; set; }
     public bool IsPublished { get; set; } = false;
+    public bool IsDeleted { get; set; } = false;
+    public DateTime? DeletedAt { get; set; }
     public string? AuthorId { get; set; }
     public User? Author { get; set; }
     public ICollection<PostTag> PostTags { get; set; } = new List<PostTag>();
