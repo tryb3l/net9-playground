050285242a311be7db0b2b716e6cfc2d9d3b2a14
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-04-16 23:58:52 +0200

    add AboutController with Index action method

---
+++ WebApplication1/Controllers/AboutController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AboutController : Controller
+    {
+        // GET: AboutController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
