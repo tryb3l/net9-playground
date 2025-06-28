cb7591bcf04d141a2e2adf3696db690045accd6a
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-06-29 00:01:21 +0200

    add DataTablesRequest and DataTablesResponse classes with model binder for handling DataTables requests and responses

---
+++ WebApplication1/Helpers/DataTablesRequest.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Helpers;
+
+[ModelBinder(typeof(DataTablesRequestModelBinder))]
+public class DataTablesRequest
+{
+    public int Draw { get; set; }
+    public int Start { get; set; }
+    public int Length { get; set; }
+    public Search? Search { get; set; }
+    public List<Column> Columns { get; set; } = new();
+    public List<Order> Order { get; set; } = new();
+}
+
+public class Search
+{
+    public string? Value { get; set; }
+    public bool Regex { get; set; }
+}
+
+public class Column
+{
+    public string? Data { get; set; }
+    public string? Name { get; set; }
+    public bool Searchable { get; set; }
+    public bool Orderable { get; set; }
+    public Search? Search { get; set; }
+}
+
+public class Order
+{
+    public int Column { get; set; }
+    public string? Dir { get; set; }
+}
---
+++ WebApplication1/Helpers/DataTablesRequestModelBinder.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace WebApplication1.Helpers;
+
+public class DataTablesRequestModelBinder : IModelBinder
+{
+    public Task BindModelAsync(ModelBindingContext bindingContext)
+    {
+        ArgumentNullException.ThrowIfNull(bindingContext);
+
+        var valueProvider = bindingContext.ValueProvider;
+
+        var model = new DataTablesRequest
+        {
+            Draw = int.Parse(valueProvider.GetValue("draw").FirstValue ?? "0"),
+            Start = int.Parse(valueProvider.GetValue("start").FirstValue ?? "0"),
+            Length = int.Parse(valueProvider.GetValue("length").FirstValue ?? "10"),
+            Search = new Search
+            {
+                Value = valueProvider.GetValue("search[value]").FirstValue,
+                Regex = bool.Parse(valueProvider.GetValue("search[regex]").FirstValue ?? "false")
+            }
+        };
+
+        var colIndex = 0;
+        while (valueProvider.GetValue($"columns[{colIndex}][data]").Any())
+        {
+            model.Columns.Add(new Column
+            {
+                Data = valueProvider.GetValue($"columns[{colIndex}][data]").FirstValue,
+                Name = valueProvider.GetValue($"columns[{colIndex}][name]").FirstValue,
+                Orderable = bool.Parse(valueProvider.GetValue($"columns[{colIndex}][orderable]").FirstValue ?? "false"),
+                Searchable = bool.Parse(valueProvider.GetValue($"columns[{colIndex}][searchable]").FirstValue ?? "false"),
+                Search = new Search
+                {
+                    Value = valueProvider.GetValue($"columns[{colIndex}][search][value]").FirstValue,
+                    Regex = bool.Parse(valueProvider.GetValue($"columns[{colIndex}][search][regex]").FirstValue ?? "false")
+                }
+            });
+            colIndex++;
+        }
+
+        var orderIndex = 0;
+        while (valueProvider.GetValue($"order[{orderIndex}][column]").Any())
+        {
+            model.Order.Add(new Order
+            {
+                Column = int.Parse(valueProvider.GetValue($"order[{orderIndex}][column]").FirstValue ?? "0"),
+                Dir = valueProvider.GetValue($"order[{orderIndex}][dir]").FirstValue
+            });
+            orderIndex++;
+        }
+
+        bindingContext.Result = ModelBindingResult.Success(model);
+        return Task.CompletedTask;
+    }
+}
