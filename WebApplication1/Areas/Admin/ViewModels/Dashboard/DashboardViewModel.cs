f28b58600c4eb6a276a082de6d675b24f4a2e5e8
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-07-01 03:01:25 +0200

    add DashboardViewModel and RecentPostViewModel for enhanced dashboard data representation

---
+++ WebApplication1/Areas/Admin/ViewModels/Dashboard/DashboardViewModel.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using WebApplication1.Models;
+
+namespace WebApplication1.Areas.Admin.ViewModels.Dashboard
+{
+    public class RecentPostViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public System.DateTime CreatedAt { get; set; }
+        public string AuthorName { get; set; }
+        public bool IsPublished { get; set; }
+    }
+
+    public class DashboardViewModel
+    {
+        public int TotalPosts { get; set; }
+        public int TotalCategories { get; set; }
+        public int TotalTags { get; set; }
+        public int TotalUsers { get; set; }
+        public IEnumerable<ActivityLog> RecentActivities { get; init; } = new List<ActivityLog>();
+        public IEnumerable<RecentPostViewModel> RecentPosts { get; set; } = new List<RecentPostViewModel>();
+    }
+}
