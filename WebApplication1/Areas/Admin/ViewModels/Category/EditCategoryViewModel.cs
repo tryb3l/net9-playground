ff52ec43786eed94efe6296e16a2483509e47352
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-07-29 00:00:11 +0200

    update EditCategoryViewModel to use init-only properties for Id and Name

--- WebApplication1/Areas/Admin/ViewModels/Category/EditCategoryViewModel.cs
+++ WebApplication1/Areas/Admin/ViewModels/Category/EditCategoryViewModel.cs
@@ -4,11 +4,11 @@ namespace WebApplication1.Areas.Admin.ViewModels.Category;
 
 public class EditCategoryViewModel
 {
-    public int Id { get; set; }
+    public int Id { get; init; }
 
     [Required]
     [StringLength(100)]
-    public required string Name { get; set; }
+    public required string Name { get; init; }
 
     [StringLength(500)]
     public string? Description { get; set; }
