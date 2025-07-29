99a7498f8f54edd474e466295979accbde7bc28b
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-07-29 23:38:42 +0200

    refactor ServiceResult to use init-only properties for better immutability

--- WebApplication1/Models/ServiceResult.cs
+++ WebApplication1/Models/ServiceResult.cs
@@ -2,8 +2,8 @@ namespace WebApplication1.Models;
 
 public class ServiceResult
 {
-    public bool Succeeded { get; set; }
-    public List<string> Errors { get; set; } = new();
+    public bool Succeeded { get; private init; }
+    public List<string> Errors { get; private init; } = [];
 
     public static ServiceResult Success() => new() { Succeeded = true };
 
