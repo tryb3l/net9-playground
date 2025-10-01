1eb849f27934f7c624fe5dccd0581f0841f8310f
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-10-01 23:57:09 +0200

    rename FeaturedImageUrl column to FeaturedImageUrls in Posts table

---
+++ WebApplication1/Data/Migrations/20250825233248_RenameFeaturedImageUrlToUrls.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication1.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameFeaturedImageUrlToUrls : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FeaturedImageUrl",
+                table: "Posts",
+                newName: "FeaturedImageUrls");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FeaturedImageUrls",
+                table: "Posts",
+                newName: "FeaturedImageUrl");
+        }
+    }
+}
