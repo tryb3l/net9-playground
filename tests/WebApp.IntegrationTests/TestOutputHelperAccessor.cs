727d4f27f7d9070996d685528a79e00898ff58d7
Bohdan Trybel <bogdan.trybel@gmail.com>
2025-11-04 00:48:49 +0100

    Added TestOutputHelperAccessor to manage test output for integration tests

---
+++ tests/WebApp.IntegrationTests/TestOutputHelperAccessor.cs
@@ -0,0 +1,30 @@
+using MartinCostello.Logging.XUnit;
+using Xunit.Sdk;
+
+namespace WebApp.IntegrationTests;
+
+public sealed class TestOutputHelperAccessor : IMessageSinkAccessor
+{
+    private ITestOutputHelper? _output;
+    
+    public IMessageSink? MessageSink { get; set; }
+
+    public void SetOutput(ITestOutputHelper output) => _output = output;
+    
+    public IMessageSink GetEffectiveSink() =>
+        MessageSink ?? (_output is null ? new NullMessageSink() : new TestOutputHelperMessageSink(_output));
+
+    private sealed class TestOutputHelperMessageSink(ITestOutputHelper output) : IMessageSink
+    {
+        public bool OnMessage(IMessageSinkMessage message)
+        {
+            output.WriteLine(message.ToString() ?? string.Empty);
+            return true;
+        }
+    }
+
+    private sealed class NullMessageSink : IMessageSink
+    {
+        public bool OnMessage(IMessageSinkMessage message) => true;
+    }
+}
