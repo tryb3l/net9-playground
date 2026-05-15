3e3996aceb3a1a6ec3e3d2e47d7b12003cf171fd
Bohdan Trybel <bogdan.trybel@gmail.com>
2026-05-16 01:25:31 +0200

    Add FeaturedImageAlt column to Posts table and update model snapshot

---
+++ src/WebApp/Data/Migrations/20260515171246_AddPostFeaturedImageAlt.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPostFeaturedImageAlt : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FeaturedImageAlt",
+                table: "Posts",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FeaturedImageAlt",
+                table: "Posts");
+        }
+    }
+}
